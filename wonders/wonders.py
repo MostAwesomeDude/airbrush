d91647402de9dd5a77fedbb8b8483b21e56a2ec9
Mike Cooper <mythmon@gmail.com>
2012-04-17 03:09:11 +0000

    Wooh, flask webapp.

--- wonders/wonders.py
+++ wonders/wonders.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
+import sys
 import random
+from flask import Flask, render_template, abort
+from pprint import pprint
 
 class Wonders(object):
 
@@ -12,10 +15,36 @@ class Wonders(object):
             self.wonders = [s.strip() for s in wonders_file]
 
     def get(self, n):
-        return self.wonders[n-1]
+        effect = self.wonders[n-1]
+        if effect[-1] not in ['.', '!', '?']:
+            effect = effect + '.'
+        return effect
 
     def random(self):
-        return random.choice(self.wonders)
+        r = random.randint(1, len(self.wonders) + 1)
+        return self.get(r)
+
+    def serve(self):
+        app = Flask(__name__)
+
+        @app.route('/')
+        def index():
+            number, effect = self.random().split(' ', 1)
+            return render_template('effect.html', effect=effect, number=number)
+
+        @app.route('/<int:n>')
+        def particular(n):
+            if n <= 0 or n >= len(self.wonders):
+                abort(404)
+            number, effect = self.get(n).split(' ', 1)
+            return render_template('effect.html', effect=effect, number=number)
+
+        @app.errorhandler(404)
+        def error(error):
+            return render_template('effect.html', effect='Wonder not found.',
+                    number=404)
+
+        app.run(debug=True)
 
 
 def main():
@@ -24,4 +53,13 @@ def main():
 
 
 if __name__ == '__main__':
-    print(main())
+    if len(sys.argv) > 1:
+        w = Wonders()
+        print {
+            'random': w.random,
+            'serve': w.serve,
+        }[sys.argv[1]](*sys.argv[2:])
+    else:
+        print('Usage: {0} command'.format(sys.argv[0]))
+        sys.exit(1)
+
