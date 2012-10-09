a77119bf5c3fe8c494f2dc97278f97153fbfe24b
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-10-08 18:37:07 -0700

    Refactor wonders to render properly.

--- airbrush.py
+++ airbrush.py
@@ -1,23 +1,35 @@
-from flask import Flask
+from flask import Flask, abort
 from flask.ext.holster.main import init_holster
 from flask.ext.holster.simple import html
 
-from wonders.wonders import Wonders
+from wonders.wonders import Wonders, prettify
 
 app = Flask(__name__)
+app.debug = True
 init_holster(app)
 
-w = Wonders("wonders/wonders.txt")
+w = Wonders()
+w.load_wonders("wonders/wonders.txt")
 
 @app.holster("/wonders")
 @app.holster("/wonders/<int:n>")
 @html("wonders.html")
 def wonders(n=None):
     if n is None:
-        effect = w.random()
+        n, effect = w.random()
     else:
-        effect = w.get(n)
+        # Switch from zero-indexed to one-indexed.
+        n -= 1
+
+        if 0 <= n < len(w.wonders):
+            effect = w.wonders[n]
+        else:
+            abort(404)
+
+    # And back to one-indexed.
+    n += 1
+
     return {
-        "effect": effect,
+        "effect": prettify(effect),
         "number": n,
     }
--- wonders/wonders.py
+++ wonders/wonders.py
@@ -1,63 +1,23 @@
 #!/usr/bin/python2
-import sys
 import random
-from flask import Flask, render_template, abort
+
+def prettify(s):
+    if s[-1] not in (".", "!", "?"):
+        s += "."
+
+    return s
 
 class Wonders(object):
 
-    def __init__(self, path):
-        self.load_wonders(path)
+    def __init__(self):
+        self.wonders = []
 
     def load_wonders(self, path):
-        self.wonders = []
-        with open(path) as wonders_file:
-            self.wonders = [s.strip() for s in wonders_file]
-
-    def get(self, n):
-        effect = self.wonders[n-1]
-        if effect[-1] not in ['.', '!', '?']:
-            effect = effect + '.'
-        return effect
+        with open(path) as f:
+            for line in f:
+                digits, rest = line.split(" ", 1)
+                self.wonders.append(rest.strip())
 
     def random(self):
-        r = random.randint(1, len(self.wonders) + 1)
-        return self.get(r)
-
-    def serve(self):
-        app = Flask(__name__)
-
-        @app.route('/')
-        def index():
-            number, effect = self.random().split(' ', 1)
-            return render_template('effect.html', effect=effect, number=number)
-
-        @app.route('/<int:n>')
-        def particular(n):
-            if n <= 0 or n >= len(self.wonders):
-                abort(404)
-            number, effect = self.get(n).split(' ', 1)
-            return render_template('effect.html', effect=effect, number=number)
-
-        @app.errorhandler(404)
-        def error(error):
-            return render_template('effect.html', effect='Wonder not found.',
-                    number=404)
-
-        app.run(debug=True)
-
-
-def main():
-    w = Wonders()
-    return w.random()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        w = Wonders()
-        print {
-            'random': w.random,
-            'serve': w.serve,
-        }[sys.argv[1]](*sys.argv[2:])
-    else:
-        print('Usage: {0} command'.format(sys.argv[0]))
-        sys.exit(1)
+        r = random.randint(0, len(self.wonders))
+        return r, self.wonders[r]
