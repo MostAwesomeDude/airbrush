b6d6c1b0adebdfab61649e126509ff5464f4a694
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-05-12 08:14:27 -0700

    tipsum: Alter algorithm to generate true sentences.
    If we find a word ending with a period (blessedly, the corpus contains no full
    stops), we insert a marker into the token stream and use that later to delimit
    chains. We also keep a list of words which can bootstrap a sentence, and
    partial chains for them. This means that sentences can now stand alone, and
    it's possible to get a good mix of both really long and really short
    sentences.

--- tipsum.py
+++ tipsum.py
@@ -6,6 +6,8 @@ from urllib import urlopen
 
 from lxml.html.soupparser import fromstring
 
+EOS = object()
+
 def make_chains():
     page = urlopen("http://timecube.com/")
     tree = fromstring(page.read())
@@ -16,22 +18,48 @@ def make_chains():
     for span in tree.xpath("//span"):
         contents = span.text_content().strip()
         for word in contents.split():
-            # There will be a couple "words" that are either NBSPs or
-            # punctuation-based HRs/BRs. Discard them with a length-based
-            # heuristic.
+            # Discard double-spaces and NBSPs.
             word = word.strip()
-            if word and len(word) < 25:
-                l.append(unicode(word.strip()))
+            if not word:
+                continue
 
-    for i, word in enumerate(l[:-3]):
-        chain = word, l[i + 1], l[i + 2]
-        chains[chain].append(l[i + 3])
+            # There will be a couple "words" that are punctuation-based
+            # HRs/BRs. Discard them with a length-based heuristic.
+            if len(word) >= 25:
+                continue
 
-    return dict(chains)
+            # Save this word.
+            l.append(unicode(word.strip()))
 
-def make_text(chains, length):
-    l = list(random.choice(list(chains)))
-    while len(l) < length:
+            if word.endswith("."):
+                # This ends a sentence; push a marker onto the stream.
+                l.append(EOS)
+
+    heads = []
+    iterator = enumerate(l[:-3])
+
+    for i, word in iterator:
+        if word is EOS:
+            # Set up a head and append a partial chain.
+            heads.append(l[i + 1])
+            chains[l[i + 1], l[i + 2]].append(l[i + 3])
+            chains[l[i + 1],].append(l[i + 2])
+        elif EOS in l[i + 1:i + 3]:
+            # Just pump it away.
+            continue
+        else:
+            chain = word, l[i + 1], l[i + 2]
+            chains[chain].append(l[i + 3])
+
+    return heads, dict(chains)
+
+def make_text(heads, chains):
+    l = [random.choice(heads)]
+    while True:
         t = tuple(l[-3:])
-        l.append(random.choice(chains[t]))
+        choice = random.choice(chains[t])
+        if choice is EOS:
+            break
+        else:
+            l.append(choice)
     return l
