e576f3eca4c968cc51dec2a3377dd6ed12071574
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-06-02 21:25:01 -0700

    analyze: Specialize AS.

--- analyze.py
+++ analyze.py
@@ -7,5 +7,11 @@ def champ_stat_at(champion, stat, level):
     The `champion` should be a dict of base statistics for the champion.
     """
 
+    if stat == "as":
+        # Attack speed is *so* weird.
+        level -= 1
+        gains = champion["asg"] * level
+        return champion["as"] * (gains + 1)
+
     gains = champion[stat + "g"] * level
     return champion[stat] + gains
